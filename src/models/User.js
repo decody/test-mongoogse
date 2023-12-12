46febbccbba5d360df4d7ee31cf03c2900b9ca00
unknown <gichulroh@gmail.com>
2023-12-12 23:39:52 +0900

    add CRUD

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     name: { 
-        firstName: { type: String, required: true },
+        firstName: { type: String, required: true},
         lastName: { type: String, required: true }
     },
     age: Number,
